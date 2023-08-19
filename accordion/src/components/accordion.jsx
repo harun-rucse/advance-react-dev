865d83c6822a1c8acf56b50185fa27524abae259
harun-rucse <horrashid007@gmail.com>
2023-08-19 11:25:43 +0600

    accordion done

--- accordion/src/components/accordion.jsx
+++ accordion/src/components/accordion.jsx
@@ -1,21 +1,38 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function Item({ item }) {
-    const [isOpen, setIsOpen] = useState(false);
+function Item({ item, currOpen, onCurrOpen }) {
+  const { id, title, text } = item;
+  const isOpen = currOpen === id;
 
-  return <div className={isOpen ? "item open": "item"} onClick={() => setIsOpen(!isOpen)}>
-    <p className="number">{item.id}</p>
-    <p className="title">{item.title}</p>
-    <p className="icon">{isOpen ? '-': '+'}</p>
-    {isOpen && <div className="content-box">{item.text}</div>}
-  </div>;
+  return (
+    <div
+      className={isOpen ? "item open" : "item"}
+      onClick={() => onCurrOpen(id)}
+    >
+      <p className="number">{id}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 }
 
 export default function Accordion({ data }) {
+  const [currOpen, setCurrOpen] = useState(null);
+
+  const handleToggleOpen = (id) => {
+    currOpen === id ? setCurrOpen(null) : setCurrOpen(id);
+  };
+
   return (
     <div className="accordion">
       {data?.map((item, index) => (
-        <Item key={index} item={{...item, id: index}} />
+        <Item
+          key={index}
+          item={{ ...item, id: index }}
+          currOpen={currOpen}
+          onCurrOpen={handleToggleOpen}
+        />
       ))}
     </div>
   );
